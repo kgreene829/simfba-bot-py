1a13d87c8c59086e84962d63c86391f669147a14
kgreene829 <65362360+kgreene829@users.noreply.github.com>
2025-01-21 04:59:35 -0500

    Update cfb_player_name_attributes.py

--- cogs/cfb_player_name_attributes.py
+++ cogs/cfb_player_name_attributes.py
@@ -24,7 +24,7 @@ class cfb_player_name_attributes(commands.Cog):
                 if stats["ID"] > 0:
                     title = f"{player['FirstName']} {player['LastName']} {stats['CollegePlayerID']}"
                 else:
-                    title = f"{player['FirstName']} {player['LastName']}"
+                    title = f"{player['FirstName']} {player['LastName']} {player['Position']}"
                 desc = f"{player['Stars']} Star {player['Year']} {player['Archetype']} {player['Position']} from {player['City']}, {player['State']}"
                 attrlist = player_builder.GetPriorityFields(player)
                 embed_player = discord.Embed(colour=discord.Colour.gold(),
