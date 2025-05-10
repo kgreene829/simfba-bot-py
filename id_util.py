15a83c6cc34c6f9ccc215ba08264f250247c134b
kgreene829 <65362360+kgreene829@users.noreply.github.com>
2025-05-10 04:50:09 -0400

    Update id_util.py

--- id_util.py
+++ id_util.py
@@ -5,6 +5,7 @@ cfb_abbrevations_json_file = os.path.join("./cfb_abbreviations.json")
 chl_abbrevations_json_file = os.path.join("./chl_abbreviations.json")
 nfl_abbrevations_json_file = os.path.join("./nfl_abbreviations.json")
 nba_abbrevations_json_file = os.path.join("./nba_abbreviations.json")
+phl_abbrevations_json_file = os.path.join("./phl_abbreviations.json")
 
 def GetCollegeHockeyTeamID(abbr):
     with open(chl_abbrevations_json_file, 'r') as f:
@@ -44,6 +45,15 @@ def GetNFLTeamID(abbr):
 
 def GetNBATeamID(abbr):
     with open(nba_abbrevations_json_file, 'r') as f:
+        data = json.load(f)
+        if abbr in data:
+            return data[abbr]
+        else:
+            no_team = "/"
+            return no_team
+
+def GetPHLTeamID(abbr):
+    with open(phl_abbrevations_json_file, 'r') as f:
         data = json.load(f)
         if abbr in data:
             return data[abbr]
